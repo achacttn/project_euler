bf0ba1c22b3f30510429cf535226471540d9d56a
Scott Lee <achacttn@gmail.com>
2018-07-03 11:06:52 +1000

    improve 10, finish 11

--- 0010/scripts.js
+++ 0010/scripts.js
@@ -1,17 +1,15 @@
-console.log('AA1');
-
+console.time('p');
 const primes = [];
-
 const isPrime = (num) => {
-
     for( var i=0; i<primes.length; i++ ){
-        if(num%primes[i]===0){
-            return false;
-        };
+        if(primes[i]<Math.sqrt(num)){
+            if(num%primes[i]===0){
+                return false;
+            };
+        }
     };
-
     var current = primes[primes.length-1]+2;
-    while( current<num ){
+    while( current<Math.sqrt(num) ){
         if( num%current===0 ){
             return false;
         }
@@ -19,12 +17,11 @@ const isPrime = (num) => {
     }
     return true
 }
-
 for( var i=2; i<2000000; i===2? i++:i+=2 ){
     if(isPrime(i)){
-        console.log(i);
+        // console.log(i);
         primes.push(i);
     }
 }
-
 console.log(primes.reduce( (a,b)=>a+b ));
+console.timeEnd('p');
